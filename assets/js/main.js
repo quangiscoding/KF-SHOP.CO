dc70c1a9d6d785ab646e9c3375cacf8dfe0dc161
quangiscoding <quangnguyen2005gt08@gmail.com>
2026-04-16 08:42:31 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+const sidebar = document.querySelector(".sidebar");
+const discount = document.querySelector(".discount");
+
+function showSideBar() {
+  sidebar.style.display = "flex";
+}
+
+function hideSideBar() {
+  sidebar.style.display = "none";
+}
+
+function hideDiscount() {
+  discount.style.display = "none";
+}
